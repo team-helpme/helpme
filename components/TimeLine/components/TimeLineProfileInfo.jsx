d4898fbfec61cbb921e32bd6f918fabbc6a768e6
Justice Otuya <jotuya2@gmail.com>
2019-05-29 00:47:22 +0100

    added the timeline components

---
+++ components/TimeLine/components/TimeLineProfileInfo.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Divider } from 'antd';
+import { USERS_BIO } from '../constant';
+import './TimeLine.css'
+
+const TimeLineProfileInfo = () => (
+    <aside className="TimeLine_profile-info">
+        <img
+            src="https://robohash.org/temporeinventorererum.bmp?size=50x50&set=set1"
+            alt="profile info of user"
+            className="user-avatar"
+        />
+        {/* followers stat */}
+        <h3 className="user-name">Baba Rahman</h3>
+        <div className="user-followers-stat">
+            <div className="users-follow-number">
+                <h3 className="count">123</h3>
+                <p>Following</p>
+            </div>
+            <Divider type="vertical" className="divider-height" />
+            <div className="users-follow-number">
+                <h3 className="count">123</h3>
+                <p>Followers</p>
+            </div>
+        </div>
+        <div className="users-bio">
+            {USERS_BIO}
+        </div>
+    </aside>
+);
+
+export default TimeLineProfileInfo;
