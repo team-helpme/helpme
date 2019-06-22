8c7672dee9f004d70bf46878fdd487e1edb3224a
Justice Otuya <jotuya2@gmail.com>
2019-06-22 14:57:50 +0100

    replaced favicon

--- config/passport.js
+++ config/passport.js
@@ -1,37 +1,37 @@
-// const passport = require('passport');
-// const Auth0Strategy = require('passport-auth0');
-// const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const dotenv = require('dotenv');
 
-// dotenv.config({ silent: true });
+dotenv.config();
 
-// // Configure Passport to use Auth0
-// const {
-//     AUTH0_CALLBACK_URL, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_DOMAIN,
-// } = process.env;
-// const strategy = new Auth0Strategy(
+// Configure Passport to use Auth0
+const {
+    AUTH0_CALLBACK_URL, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_DOMAIN,
+} = process.env;
+const strategy = new Auth0Strategy(
 
-//     {
-//         callbackURL: AUTH0_CALLBACK_URL || '/auth/signed-in',
-//         clientID: AUTH0_CLIENT_ID,
-//         clientSecret: AUTH0_CLIENT_SECRET,
-//         domain: AUTH0_DOMAIN,
-//     },
-//     (accessToken, refreshToken, extraParams, profile, done) => {
-//         // accessToken is the token to call Auth0 API (not needed in the most cases)
-//         // extraParams.id_token has the JSON Web Token
-//         // profile has all the information from the user
+    {
+        callbackURL: AUTH0_CALLBACK_URL || '/auth/signed-in',
+        clientID: AUTH0_CLIENT_ID,
+        clientSecret: AUTH0_CLIENT_SECRET,
+        domain: AUTH0_DOMAIN,
+    },
+    (accessToken, refreshToken, extraParams, profile, done) => {
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
+        // extraParams.id_token has the JSON Web Token
+        // profile has all the information from the user
 
-//         done(null, profile);
-//     }
-// );
+        done(null, profile);
+    }
+);
 
-// passport.use(strategy);
+passport.use(strategy);
 
-// // You can use this section to keep a smaller payload
-// passport.serializeUser((user, done) => {
-//     done(null, user);
-// });
+// You can use this section to keep a smaller payload
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
 
-// passport.deserializeUser((user, done) => {
-//     done(null, user);
-// });
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
