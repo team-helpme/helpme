baeee286273f3db28d8d9f00de1331c01f24161a
Justice Otuya <jotuya2@gmail.com>
2019-06-22 14:27:48 +0100

    removed favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,10 +10,11 @@ export default class extends Document {
             <html lang="en">
                 <Head>
                     <meta charSet="UTF-8" />
+                    <meta httpEquiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://www.google.com" />
                     <meta name="viewport" content="width=device-width initial-scale=1.0" />
                     <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-                    {/* <link rel="shortcut icon" href={FAVICON} type="image/x-icon" />
-                    <link rel="icon" href={FAVICON} type="image/x-icon" /> */}
+                    <link rel="shortcut icon" href={FAVICON} type="image/x-icon" />
+                    <link rel="icon" href={FAVICON} type="image/x-icon" />
                     <link
                         rel="stylesheet"
                         href={href}
